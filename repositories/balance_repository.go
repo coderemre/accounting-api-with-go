753f523cda9cd5af0a3c9cbc220dbd8996f59d64
Emre Babur <benemrebabur@gmail.com>
2025-03-11 13:47:33 +0300

    fix: deleted unused files

--- repositories/balance_repository.go
+++
@@ -1,8 +0,0 @@
-package repositories
-
-import "accounting-api-with-go/internal/models"
-
-type BalanceRepository interface {
-	GetBalance(userID int64) (*models.Balance, error)
-	UpdateBalance(b *models.Balance) error
-}
